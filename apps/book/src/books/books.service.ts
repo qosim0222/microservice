1d08c8a8c1497938043d5a57edc1cb1c746665ad
qosim0222 <qosimxushvaxtov78@gmail.com>
2025-04-10 12:25:40 +0500

    dadad

--- apps/book/src/books/books.service.ts
+++ apps/book/src/books/books.service.ts
@@ -4,23 +4,46 @@ import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BooksService {
-  create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+  private books: CreateBookDto[] = [];
+
+  create(createBookDto: Omit<CreateBookDto, 'id'>): CreateBookDto {
+    const newBook: CreateBookDto = {
+      ...createBookDto,
+      id: this.books.length > 0 ? this.books.at(-1)!.id + 1 : 1,
+    };
+    this.books.push(newBook);
+    return newBook;
   }
 
   findAll() {
-    return `This action returns all books`;
+    return this.books;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} book`;
+    return this.books.find((book) => book.id === id);
   }
 
   update(id: number, updateBookDto: UpdateBookDto) {
-    return `This action updates a #${id} book`;
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex === -1) return { message: "Book not found!" };
+
+    this.books[bookIndex] = { ...this.books[bookIndex], ...updateBookDto };
+    return {
+      message: "Book updated successfully",
+      book: this.books[bookIndex],
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} book`;
+    const bookIndex = this.books.findIndex((book) => book.id === id);
+    if (bookIndex === -1) return { message: "Book not found!" };
+
+    const removedBook = this.books[bookIndex];
+    this.books.splice(bookIndex, 1);
+
+    return {
+      message: "Book removed successfully",
+      book: removedBook,
+    };
   }
 }
--- apps/user/src/users/users.service.ts
+++ apps/user/src/users/users.service.ts
@@ -3,24 +3,49 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UserService {
+  private users: CreateUserDto[] = [];
+
+  create(createUserDto: Omit<CreateUserDto, 'id'>): CreateUserDto {
+    const newUser: CreateUserDto = {
+      ...createUserDto,
+      id: this.users.length > 0 ? this.users.at(-1)!.id + 1 : 1,
+    };
+    this.users.push(newUser);
+    return newUser;
   }
 
+
+
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.users.find((user) => user.id === id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) return { message: "Not found!" };
+
+    this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+    return {
+      message: "User updated successfully",
+      user: this.users[userIndex],
+    };
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) return { message: "User not found!" };
+
+    const removedUser = this.users[userIndex];
+    this.users.splice(userIndex, 1);
+
+    return {
+      message: "User removed successfully",
+      user: removedUser,
+    };
   }
 }
